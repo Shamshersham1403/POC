8d691e233e04ab4f717bd80214c16afe82db7577
ShamsherAhmad <shamshersham1403@gmail.com>
2021-06-24 16:39:08 +0530

    POC-Task 2 Spring security

---
+++ studentSecurity/src/test/java/com/poc/studentSecurity/StudentSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.studentSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
